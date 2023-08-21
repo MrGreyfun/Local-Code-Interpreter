485ee03dab906adefaaa1a1acf5e0743d6d53468
MrGreyfun <1078210504@qq.com>
2023-08-21 19:38:19 +0800

    Replace previous bot message recording approach withBotBackendLog class

---
+++ src/bot_backend.py
@@ -0,0 +1,247 @@
+from jupyter_backend import *
+from utils import *
+import base64
+import copy
+
+
+def chat_completion(bot_backend_log):
+    model_choice = bot_backend_log.gpt_model_choice
+    config = bot_backend_log.config
+    kwargs_for_chat_completion = bot_backend_log.kwargs_for_chat_completion
+
+    assert config['model'][model_choice]['available'], f"{model_choice} is not available for you API key"
+
+    response = openai.ChatCompletion.create(**kwargs_for_chat_completion)
+    return response
+
+
+def send_output(content_to_display, history):
+    images, text = [], []
+
+    # terminal output
+    error_occurred = False
+    for mark, out_str in content_to_display:
+        if mark in ('stdout', 'execute_result_text', 'display_text'):
+            text.append(out_str)
+        elif mark in (
+                'execute_result_png', 'execute_result_jpeg', 'display_png', 'display_jpeg'):
+            if 'png' in mark:
+                images.append(('png', out_str))
+            else:
+                images.append(('jpg', out_str))
+        elif mark == 'error':
+            text.append(delete_color_control_char(out_str))
+            error_occurred = True
+    text = '\n'.join(text).strip('\n')
+    if error_occurred:
+        history.append([None, f'❌Terminal output:\n```shell\n\n{text}\n```'])
+    else:
+        history.append([None, f'✔️Terminal output:\n```shell\n{text}\n```'])
+
+    # image output
+    for idx, (filetype, img) in enumerate(images):
+        image_bytes = base64.b64decode(img)
+        if not os.path.exists('temp'):
+            os.mkdir('temp')
+        path = f'temp/{idx}.{filetype}'
+        with open(path, 'wb') as f:
+            f.write(image_bytes)
+        history.append([None, (path,)])
+
+
+def parse_json(function_args: str, finished: bool):
+    """
+    GPT may generate non-standard JSON format string, which contains '\n' in string value, leading to error when using
+    `json.loads()`.
+    Here we implement a parser to extract code directly from non-standard JSON string.
+    :return: code string if successfully parsed otherwise None
+    """
+    parser_log = {
+        'met_begin_{': False,
+        'begin_"code"': False,
+        'end_"code"': False,
+        'met_:': False,
+        'met_end_}': False,
+        'met_end_code_"': False,
+        "code_begin_index": 0,
+        "code_end_index": 0
+    }
+    try:
+        for index, char in enumerate(function_args):
+            if char == '{':
+                parser_log['met_begin_{'] = True
+            elif parser_log['met_begin_{'] and char == '"':
+                if parser_log['met_:']:
+                    if finished:
+                        parser_log['code_begin_index'] = index + 1
+                        break
+                    else:
+                        if index + 1 == len(function_args):
+                            return ''
+                        else:
+                            temp_code_str = function_args[index + 1:]
+                            if '\n' in temp_code_str:
+                                return temp_code_str.strip('\n')
+                            else:
+                                return json.loads(function_args + '"}')['code']
+                elif parser_log['begin_"code"']:
+                    parser_log['end_"code"'] = True
+                else:
+                    parser_log['begin_"code"'] = True
+            elif parser_log['end_"code"'] and char == ':':
+                parser_log['met_:'] = True
+            else:
+                continue
+        if finished:
+            for index, char in enumerate(function_args[::-1]):
+                back_index = -1 - index
+                if char == '}':
+                    parser_log['met_end_}'] = True
+                elif parser_log['met_end_}'] and char == '"':
+                    parser_log['code_end_index'] = back_index - 1
+                    break
+                else:
+                    continue
+            code_str = function_args[parser_log['code_begin_index']: parser_log['code_end_index'] + 1]
+            if '\n' in code_str:
+                return code_str.strip('\n')
+            else:
+                return json.loads(function_args)['code']
+
+    except Exception as e:
+        return None
+
+
+def parse_response(chunk, history, bot_backend_log, function_dict):
+    """
+    :return: history, whether_exit
+    """
+    gpt_api_log = bot_backend_log.gpt_api_log
+    conversation = bot_backend_log.conversation
+
+    whether_exit = False
+    if chunk['choices']:
+        delta = chunk['choices'][0]['delta']
+        if 'role' in delta:
+            gpt_api_log['assistant_role_name'] = delta['role']
+        if 'content' in delta:
+            if delta['content'] is not None:
+                # null value of content often occur in function call:
+                #     {
+                #       "role": "assistant",
+                #       "content": null,
+                #       "function_call": {
+                #         "name": "python",
+                #         "arguments": ""
+                #       }
+                #     }
+                gpt_api_log['content'] += delta.get('content', '')
+                history[-1][1] = gpt_api_log['content']
+
+        if 'function_call' in delta:
+            if 'name' in delta['function_call']:
+                gpt_api_log['function_name'] = delta['function_call']['name']
+                gpt_api_log['content_history'] = copy.deepcopy(history)
+                if gpt_api_log['function_name'] not in function_dict:
+                    history.append(
+                        [
+                            None,
+                            f'GPT attempted to call a function that does '
+                            f'not exist: {gpt_api_log["function_name"]}\n '
+                        ]
+                    )
+                    whether_exit = True
+
+                    return history, whether_exit
+
+            if 'arguments' in delta['function_call']:
+                gpt_api_log['function_args_str'] += delta['function_call']['arguments']
+
+                if gpt_api_log['function_name'] == 'python':  # handle hallucinatory function calls
+                    '''
+                    In practice, we have noticed that GPT, especially GPT-3.5, may occasionally produce hallucinatory
+                    function calls. These calls involve a non-existent function named `python` with arguments consisting 
+                    solely of raw code text (not a JSON format).
+                    '''
+                    temp_code_str = gpt_api_log['function_args_str']
+                    gpt_api_log['display_code_block'] = "\n🔴Working:\n```python\n{}\n```".format(temp_code_str)
+                    history = copy.deepcopy(gpt_api_log['content_history'])
+                    history[-1][1] += gpt_api_log['display_code_block']
+                else:
+                    temp_code_str = parse_json(function_args=gpt_api_log['function_args_str'], finished=False)
+                    if temp_code_str is not None:
+                        gpt_api_log['display_code_block'] = "\n🔴Working:\n```python\n{}\n```".format(
+                            temp_code_str
+                        )
+                        history = copy.deepcopy(gpt_api_log['content_history'])
+                        history[-1][1] += gpt_api_log['display_code_block']
+
+        if chunk['choices'][0]['finish_reason'] is not None:
+            if gpt_api_log['content']:
+                conversation.append(
+                    {'role': gpt_api_log['assistant_role_name'], 'content': gpt_api_log['content']}
+                )
+
+            gpt_api_log['finish_reason'] = chunk['choices'][0]['finish_reason']
+            if gpt_api_log['finish_reason'] == 'function_call':
+                try:
+                    if gpt_api_log['function_name'] == 'python':
+                        code_str = gpt_api_log['function_args_str']
+                    else:
+                        code_str = parse_json(function_args=gpt_api_log['function_args_str'], finished=True)
+                        if code_str is None:
+                            raise json.JSONDecodeError
+                    gpt_api_log['display_code_block'] = "\n🟢Working:\n```python\n{}\n```".format(code_str)
+                    history = copy.deepcopy(gpt_api_log['content_history'])
+                    history[-1][1] += gpt_api_log['display_code_block']
+
+                    # function response
+                    text_to_gpt, content_to_display = function_dict[
+                        gpt_api_log['function_name']
+                    ](code_str)
+
+                    # add function call to conversion
+                    conversation.append(
+                        {
+                            "role": gpt_api_log["assistant_role_name"],
+                            "name": gpt_api_log["function_name"],
+                            "content": gpt_api_log["function_args_str"],
+                        }
+                    )
+
+                    if len(text_to_gpt) > 500:
+                        text_to_gpt = f'{text_to_gpt[:200]}\n[Output too much, the middle part output is omitted]\n ' \
+                                      f'End part of output:\n{text_to_gpt[-200:]}'
+                    conversation.append(
+                        {
+                            "role": "function",
+                            "name": gpt_api_log["function_name"],
+                            "content": text_to_gpt,
+                        }
+                    )
+
+                    send_output(content_to_display=content_to_display, history=history)
+
+                except json.JSONDecodeError:
+                    history.append(
+                        [None, f"GPT generate wrong function args: {gpt_api_log['function_args_str']}"]
+                    )
+                    whether_exit = True
+                    return history, whether_exit
+
+                except Exception as e:
+                    history.append([None, f'Backend error: {e}'])
+                    whether_exit = True
+                    return history, whether_exit
+
+            gpt_api_log.update(
+                {
+                    "assistant_role_name": "",
+                    "content": "",
+                    "function_name": None,
+                    "function_args_str": "",
+                    "display_code_block": "",
+                }
+            )
+
+    return history, whether_exit
--- src/web_ui.py
+++ src/web_ui.py
@@ -1,50 +1,46 @@
-from utils import *
+from bot_backend import *
 import gradio as gr
 import shutil
 
-gpt_results = {
-    "assistant_role_name": "",
-    "content": "",
-    "function_name": None,
-    "function_args_str": "",
-    "display_code_block": "",
-    "finish_reason": "stop",
-    "parser_wait": 5,
-    "content_history": None
-}
-
-model = "GPT-3.5"
-
-revocable_files = []
-
 
 def switch_to_gpt4(whether_switch):
-    global model
+    global bot_backend_log
     if whether_switch:
-        model = "GPT-4"
+        bot_backend_log.update_gpt_model_choice("GPT-4")
     else:
-        model = "GPT-3.5"
+        bot_backend_log.update_gpt_model_choice("GPT-3.5")
 
 
-def add_text(history: List, text: str):
-    global gpt_results
+def add_text(history, text: str):
+    global bot_backend_log
+    conversation = bot_backend_log.conversation
+    revocable_files = bot_backend_log.revocable_files
+    gpt_api_log = bot_backend_log.gpt_api_log
+
     revocable_files.clear()
     history = history + [(text, None)]
     conversation.append(
         {'role': 'user', 'content': text}
     )
-    gpt_results['finish_reason'] = 'new_input'
+    gpt_api_log['finish_reason'] = 'new_input'
+
     return history, gr.update(value="", interactive=False)
 
 
-def add_file(history: List, file):
+def add_file(history, file):
+    global bot_backend_log
+    revocable_files = bot_backend_log.revocable_files
+    conversation = bot_backend_log.conversation
     path = file.name
     filename = os.path.basename(path)
+
     shutil.copy(path, 'work_dir')
+
     bot_conversation = [f'📁[{filename}]', None]
     gpt_conversation = {'role': 'system', 'content': f'User uploaded a file: {filename}'}
     history.append(bot_conversation)
     conversation.append(gpt_conversation)
+
     revocable_files.append(
         {
             'bot_conversation': bot_conversation,
@@ -54,7 +50,11 @@ def add_file(history: List, file):
     return history
 
 
-def undo_upload_file(history: List):
+def undo_upload_file(history):
+    global bot_backend_log
+    revocable_files = bot_backend_log.revocable_files
+    conversation = bot_backend_log.conversation
+
     if revocable_files:
         file = revocable_files[-1]
         bot_conversation = file['bot_conversation']
@@ -71,10 +71,11 @@ def undo_upload_file(history: List):
         return history, gr.Button.update(interactive=False)
 
 
-def bot(history: List):
-    global gpt_results
+def bot(history):
+    global bot_backend_log
+    gpt_api_log = bot_backend_log.gpt_api_log
 
-    while gpt_results['finish_reason'] in ('new_input', 'function_call'):
+    while gpt_api_log['finish_reason'] in ('new_input', 'function_call'):
         if history[-1][0] is None:
             history.append(
                 [None, ""]
@@ -82,12 +83,12 @@ def bot(history: List):
         else:
             history[-1][1] = ""
 
-        response = chat_completion(model_choice=model)
+        response = chat_completion(bot_backend_log=bot_backend_log)
         for chunk in response:
             history, weather_exit = parse_response(
                 chunk=chunk,
                 history=history,
-                gpt_results=gpt_results,
+                bot_backend_log=bot_backend_log,
                 function_dict=available_functions
             )
             yield history
@@ -97,38 +98,41 @@ def bot(history: List):
     yield history
 
 
-with gr.Blocks(theme=gr.themes.Base()) as block:
-    """
-    Reference: https://www.gradio.app/guides/creating-a-chatbot-fast
-    """
-    chatbot = gr.Chatbot([], elem_id="chatbot").style(height=750)
+if __name__ == '__main__':
+    bot_backend_log = BotBackendLog()
+    config = bot_backend_log.config
 
-    with gr.Row():
-        with gr.Column(scale=0.85):
-            text_box = gr.Textbox(
-                show_label=False,
-                placeholder="Enter text and press enter, or upload a file",
-            ).style(container=False)
-        with gr.Column(scale=0.15, min_width=0):
-            file_upload_button = gr.UploadButton("📁", file_types=['file'])
+    with gr.Blocks(theme=gr.themes.Base()) as block:
+        """
+        Reference: https://www.gradio.app/guides/creating-a-chatbot-fast
+        """
+        chatbot = gr.Chatbot([], elem_id="chatbot").style(height=750)
+        with gr.Row():
+            with gr.Column(scale=0.85):
+                text_box = gr.Textbox(
+                    show_label=False,
+                    placeholder="Enter text and press enter, or upload a file",
+                ).style(container=False)
+            with gr.Column(scale=0.15, min_width=0):
+                file_upload_button = gr.UploadButton("📁", file_types=['file'])
 
-    with gr.Row(equal_height=True):
-        with gr.Column(scale=0.85):
-            check_box = gr.Checkbox(label="Using GPT-4", interactive=config['model']['GPT-4']['available'])
-            check_box.change(fn=switch_to_gpt4, inputs=check_box)
-        with gr.Column(scale=0.15, min_width=0):
-            undo_file_button = gr.Button(value="↩️Undo upload file", interactive=False)
+        with gr.Row(equal_height=True):
+            with gr.Column(scale=0.85):
+                check_box = gr.Checkbox(label="Using GPT-4", interactive=config['model']['GPT-4']['available'])
+                check_box.change(fn=switch_to_gpt4, inputs=check_box)
+            with gr.Column(scale=0.15, min_width=0):
+                undo_file_button = gr.Button(value="↩️Undo upload file", interactive=False)
 
-    txt_msg = text_box.submit(add_text, [chatbot, text_box], [chatbot, text_box], queue=False).then(
-        bot, chatbot, chatbot
-    )
-    txt_msg.then(lambda: gr.update(interactive=True), None, [text_box], queue=False)
-    txt_msg.then(lambda: gr.Button.update(interactive=False), None, [undo_file_button], queue=False)
-    file_msg = file_upload_button.upload(add_file, [chatbot, file_upload_button], [chatbot], queue=False).then(
-        bot, chatbot, chatbot
-    )
-    file_msg.then(lambda: gr.Button.update(interactive=True), None, [undo_file_button], queue=False)
-    undo_file_button.click(fn=undo_upload_file, inputs=[chatbot], outputs=[chatbot, undo_file_button])
+        txt_msg = text_box.submit(add_text, [chatbot, text_box], [chatbot, text_box], queue=False).then(
+            bot, chatbot, chatbot
+        )
+        txt_msg.then(lambda: gr.update(interactive=True), None, [text_box], queue=False)
+        txt_msg.then(lambda: gr.Button.update(interactive=False), None, [undo_file_button], queue=False)
+        file_msg = file_upload_button.upload(add_file, [chatbot, file_upload_button], [chatbot], queue=False).then(
+            bot, chatbot, chatbot
+        )
+        file_msg.then(lambda: gr.Button.update(interactive=True), None, [undo_file_button], queue=False)
+        undo_file_button.click(fn=undo_upload_file, inputs=[chatbot], outputs=[chatbot, undo_file_button])
 
-block.queue()
-block.launch()
+    block.queue()
+    block.launch(inbrowser=True)
